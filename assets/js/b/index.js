ea733f8af84e74e9d9e4ed6013b2f7b49be8dd57
CusMeDroid <108187260+CusMeDroid@users.noreply.github.com>
2024-04-01 02:02:21 +0700

    Add files via upload

---
+++ assets/js/b/index.js
@@ -0,0 +1,5 @@
+function close_window() {
+  if (confirm("Close Window?")) {
+    close();
+  }
+}
--- assets/js/playlist/index.js
+++ assets/js/playlist/index.js
@@ -1,10 +1,6 @@
 var iconImage = "https://firebasestorage.googleapis.com/v0/b/overtunes-music.appspot.com/o/logo%2Ffavicon.ico?alt=media&token=c9c5bc2e-da6c-4a2b-90ad-53dfe40e34da";
 AP.init({
   playList: [
-    {"icon": iconImage, "title": "Feat Stefani & Bayu - Bersembunyi", "file": "https://github.com/CusMeDroid/radiotipmasalampau/raw/main/music/RTML%20Feat%20Bayu%20%26%20Stefani%20-%20Bersembunyi.mp3"},
-    {"icon": iconImage, "title": "CinLok", "file": "https://github.com/CusMeDroid/radiotipmasalampau/raw/main/music/Radio%20Tip%20Masa%20Lampau%20-%20CinLok.mp3"},
-    {"icon": iconImage, "title": "Gelap Hariku", "file": "https://github.com/CusMeDroid/radiotipmasalampau/raw/main/music/Radio%20Tip%20Masa%20Lampau%20-%20Gelap%20HariKu.mp3"},
-    {"icon": iconImage, "title": "Hanya Mimpi", "file": "https://github.com/CusMeDroid/radiotipmasalampau/raw/main/music/Radio%20Tip%20Masa%20Lampau%20-%20Hanya%20Mimpi.mp3"},
-    {"icon": iconImage, "title": "Sudah Berakhir", "file": "https://github.com/CusMeDroid/radiotipmasalampau/raw/main/music/Radio%20Tip%20Masa%20Lampau%20-%20Sudah%20Berakhir.mp3"}
+    {"icon": iconImage, "Radio Tip Masa Lampau - CinLok", "file": "../song/rtml_cinlok.mp3"}
   ]
 });
