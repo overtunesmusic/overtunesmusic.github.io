6c84caaed43608dea78978976b4aa3116ca7999c
CusMeDroid <108187260+CusMeDroid@users.noreply.github.com>
2024-04-01 02:05:18 +0700

    Update index.js

--- assets/js/playlist/index.js
+++ assets/js/playlist/index.js
@@ -1,6 +1,8 @@
 var iconImage = "https://firebasestorage.googleapis.com/v0/b/overtunes-music.appspot.com/o/logo%2Ffavicon.ico?alt=media&token=c9c5bc2e-da6c-4a2b-90ad-53dfe40e34da";
 AP.init({
   playList: [
-    {"icon": iconImage, "Radio Tip Masa Lampau - CinLok", "file": "../song/rtml_cinlok.mp3"}
+    {"icon": iconImage, "Hope In Holiday - Hurry Up", "file": "https://github.com/overtunesmusic/overtunesmusic.github.io/raw/main/assets/song/Hope%20In%20Holiday%20-%20Hurry%20Up.mp3"},
+    {"icon": iconImage, "Denniar Long Feat Bakat - Risalah Hati (Cover)", "file": "https://github.com/overtunesmusic/overtunesmusic.github.io/raw/main/assets/song/Denniar%20Long%20Feat%20Bakat%20-%20Risalah%20Hati%20(Cover).mp3"},
+    {"icon": iconImage, "Radio Tip Masa Lampau - CinLok", "file": "https://github.com/overtunesmusic/overtunesmusic.github.io/raw/main/assets/song/Radio%20Tip%20Masa%20Lampau%20-%20CinLok.mp3"}
   ]
 });
