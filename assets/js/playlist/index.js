c3025bc17526910b6775f8cb2fada75a156e028b
CusMeDroid <108187260+CusMeDroid@users.noreply.github.com>
2024-04-01 02:41:03 +0700

    Update index.js

--- assets/js/playlist/index.js
+++ assets/js/playlist/index.js
@@ -1,8 +1,8 @@
-var iconImage = "https://firebasestorage.googleapis.com/v0/b/overtunes-music.appspot.com/o/logo%2Ffavicon.ico?alt=media&token=c9c5bc2e-da6c-4a2b-90ad-53dfe40e34da";
+var iconImage = 'https://firebasestorage.googleapis.com/v0/b/overtunes-music.appspot.com/o/logo%2Ffavicon.ico?alt=media&token=c9c5bc2e-da6c-4a2b-90ad-53dfe40e34da';
 AP.init({
   playList: [
-    {"icon": iconImage, "Hope In Holiday - Hurry Up", "file": "https://github.com/overtunesmusic/overtunesmusic.github.io/raw/main/assets/song/Hope%20In%20Holiday%20-%20Hurry%20Up.mp3"},
-    {"icon": iconImage, "Denniar Long Feat Bakat - Risalah Hati (Cover)", "file": "https://github.com/overtunesmusic/overtunesmusic.github.io/raw/main/assets/song/Denniar%20Long%20Feat%20Bakat%20-%20Risalah%20Hati%20(Cover).mp3"},
-    {"icon": iconImage, "Radio Tip Masa Lampau - CinLok", "file": "https://github.com/overtunesmusic/overtunesmusic.github.io/raw/main/assets/song/Radio%20Tip%20Masa%20Lampau%20-%20CinLok.mp3"}
+    {'icon': iconImage, 'title': 'Hope In Holiday - Hurry Up', 'file': 'https://github.com/overtunesmusic/overtunesmusic.github.io/raw/main/song/Hope%20In%20Holiday%20-%20Hurry%20Up.mp3'},
+    {'icon': iconImage, 'title': 'Denniar Long Feat Bakat - Risalah Hati (Cover)', 'file': 'https://github.com/overtunesmusic/overtunesmusic.github.io/raw/main/song/Denniar%20Long%20Feat%20Bakat%20-%20Risalah%20Hati%20(Cover).mp3'},
+    {'icon': iconImage, 'title': 'Radio Tip Masa Lampau - CinLok', 'file': 'https://github.com/overtunesmusic/overtunesmusic.github.io/raw/main/song/Radio%20Tip%20Masa%20Lampau%20-%20CinLok.mp3'}
   ]
 });
