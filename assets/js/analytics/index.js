a7b15ef39ac64f7ef107e7d31e72582a7902ec02
Overtunes Music <165518104+overtunesmusic@users.noreply.github.com>
2024-04-01 20:33:34 +0700

    Update index.js

--- assets/js/analytics/index.js
+++ assets/js/analytics/index.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-analytics.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
