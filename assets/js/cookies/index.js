d10ca61b5b4b2215b0ae7a703135696349a2f22b
CusMeDroid <108187260+CusMeDroid@users.noreply.github.com>
2024-04-02 05:06:54 +0700

    Update index.js

--- assets/js/cookies/index.js
+++ assets/js/cookies/index.js
@@ -2,7 +2,7 @@ function setCookie(cname, cvalue, exdays) {
     const d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
     let expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/" + "SameSite" + "Lax";
 }
 
 function getCookie(cname) {
@@ -28,11 +28,13 @@ if (user != "") {
     user = "true";
     if (user != "" && user != null) {
         setCookie("cookies", user, 365);
+        Set-Cookie: promo_shown=1; SameSite=Lax
         document.getElementById("main-cookies").style.display = "none";
     }
 }
 
 document.getElementById("btn-cookies").onclick = function () {
+    let user = getCookie("cookies");
     if (user != "") {
         // console.log("Welcome again " + user);
         document.getElementById("main-cookies").style.display = "none";
@@ -40,6 +42,7 @@ document.getElementById("btn-cookies").onclick = function () {
         user = "true";
         if (user != "" && user != null) {
             setCookie("cookies", user, 365);
+            Set-Cookie: promo_shown=1; SameSite=Lax
             document.getElementById("main-cookies").style.display = "none";
         }
     }
