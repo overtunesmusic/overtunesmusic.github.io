9f1116c31c7d4e304ff61259b376ce233431e929
CusMeDroid <108187260+CusMeDroid@users.noreply.github.com>
2024-04-03 00:09:14 +0700

    Update index.js

--- assets/js/playlist/index.js
+++ assets/js/playlist/index.js
@@ -1,9 +1,26 @@
-var iconImage = 'https://firebasestorage.googleapis.com/v0/b/overtunes-music.appspot.com/o/logo%2Ffavicon.ico?alt=media&token=c9c5bc2e-da6c-4a2b-90ad-53dfe40e34da';
+var iconImage = "https://firebasestorage.googleapis.com/v0/b/overtunes-music.appspot.com/o/logo%2Ffavicon.ico?alt=media&token=c9c5bc2e-da6c-4a2b-90ad-53dfe40e34da";
 AP.init({
   playList: [
-    {'icon': iconImage, 'title': 'Hope In Holiday - Hurry Up', 'file': 'https://github.com/overtunesmusic/overtunesmusic.github.io/raw/main/song/Hope%20In%20Holiday%20-%20Hurry%20Up.mp3'},
-    {'icon': iconImage, 'title': 'Denniar Long Feat Bakat - Risalah Hati (Cover)', 'file': 'https://github.com/overtunesmusic/overtunesmusic.github.io/raw/main/song/Denniar%20Long%20Feat%20Bakat%20-%20Risalah%20Hati%20(Cover).mp3'},
-    {'icon': iconImage, 'title': 'Eki Manna & IyoRTML Ft. Mery S - Cintaku Jangan Kau Benci', 'file': 'https://github.com/overtunesmusic/overtunesmusic.github.io/raw/main/song/Eki%20Manna%20&%20IyoRTML%20Ft.%20Mery%20S%20-%20Cintaku%20Jangan%20Kau%20Benci.mp3'},
-    {'icon': iconImage, 'title': 'Radio Tip Masa Lampau - CinLok', 'file': 'https://github.com/overtunesmusic/overtunesmusic.github.io/raw/main/song/Radio%20Tip%20Masa%20Lampau%20-%20CinLok.mp3'}
+    {"icon": iconImage, "title": "ALEANOR - MACAPAD DURMA", "file": "https://github.com/overtunesmusic/overtunesmusic.github.io/raw/main/song/ALEANOR-MACAPAD%20DURMA.mp3"},
+    {"icon": iconImage, "title": "BAKAT - JANGAN BERSEDIH (Fluffy Cover)", "file": "https://github.com/overtunesmusic/overtunesmusic.github.io/raw/main/song/BAKAT%20-%20JANGAN%20BERSEDIH%20(%20Fluffy%20Cover).mp3"},
+    {"icon": iconImage, "title": "BIRTH OF INNOCENCE - HILANG", "file": "https://github.com/overtunesmusic/overtunesmusic.github.io/raw/main/song/BIRTH%20OF%20INNOCENCE%20-%20HILANG.mp3"},
+    {"icon": iconImage, "title": "CAR DRIVES AWAY - COMEBACK HOME", "file": "https://github.com/overtunesmusic/overtunesmusic.github.io/raw/main/song/CAR%20DRIVES%20AWAY%20-COMEBACK%20HOME.mp3"},
+    {"icon": iconImage, "title": "DENNIAR LONG FEAT M ARIFIN ZEIN - BEAUTIFUL (Cover)", "file": "https://github.com/overtunesmusic/overtunesmusic.github.io/raw/main/song/DENNIAR%20LONG%20FEAT%20M%20ARIFIN%20ZEIN%20%20-%20%20BEAUTIFUL%20(Cover).mp3"},
+    {"icon": iconImage, "title": "DENNIAR LONG FEAT M ARIFIN ZEIN - SO SICK (Neyoo Cover) NEW", "file": "https://github.com/overtunesmusic/overtunesmusic.github.io/raw/main/song/DENNIAR%20LONG%20FEAT%20M%20ARIFIN%20ZEIN%20%20-%20SO%20SICK%20(Neyoo%20Cover)%20NEW.mp3"},
+    {"icon": iconImage, "title": "FLUFFY - BUKAN YANG TERBAIK (Akustik)", "file": "https://github.com/overtunesmusic/overtunesmusic.github.io/raw/main/song/FLUFFY%20-%20BUKAN%20YANG%20TERBAIK%20(Akustik).mp3"},
+    {"icon": iconImage, "title": "HISTORY OF STRUGGLE - DESTROY THE ATMOSPHERE", "file": "https://github.com/overtunesmusic/overtunesmusic.github.io/raw/main/song/HISTORY%20OF%20STRUGGLE%20-%20DESTROY%20THE%20ATMOSPHERE.mp3"},
+    {"icon": iconImage, "title": "HISTORY OF STRUGGLE - INTRO", "file": "https://github.com/overtunesmusic/overtunesmusic.github.io/raw/main/song/HISTORY%20OF%20STRUGGLE-INTRO.mp3"},
+    {"icon": iconImage, "title": "HOLIDAY IN HOLIDAY - HURRY UP", "file": "https://github.com/overtunesmusic/overtunesmusic.github.io/raw/main/song/HOLIDAY%20IN%20HOLIDAY%20-%20HURRY%20UP.mp3"},
+    {"icon": iconImage, "title": "MALE FOUR DUCK - KULIAH PAGI", "file": "https://github.com/overtunesmusic/overtunesmusic.github.io/raw/main/song/MALE%20FOUR%20DUCK%20-%20KULIAH%20PAGI.mp3"},
+    {"icon": iconImage, "title": "RIVKY - A PROMISE (COVER)", "file": "https://github.com/overtunesmusic/overtunesmusic.github.io/raw/main/song/RIVKY%20-%20A%20PROMISE%20(COVER).mp3"},
+    {"icon": iconImage, "title": "ROOF THE STREETS  Feat M ARIFIN ZEIN - WAKE UP! YOU'RE AWESOME! (Akustik)", "file": "https://github.com/overtunesmusic/overtunesmusic.github.io/raw/main/song/ROOF%20THE%20STREETS%20%20Feat%20M%20ARIFIN%20ZEIN%20-%20WAKE%20UP!%20YOU'RE%20AWESOME!%20(%20Akustik%20).mp3"},
+    {"icon": iconImage, "title": "ROOF THE STREETS - I HOPE, MY WAY IN TIME", "file": "https://github.com/overtunesmusic/overtunesmusic.github.io/raw/main/song/ROOF%20THE%20STREETS%20-%20I%20HOPE%20,%20MY%20WAY%20IN%20TIME.mp3"},
+    {"icon": iconImage, "title": "ROOF THE STREETS - WAKE UP! YOU'RE AWESOME!", "file": "https://github.com/overtunesmusic/overtunesmusic.github.io/raw/main/song/ROOF%20THE%20STREETS%20-%20WAKE%20UP!%20YOU'RE%20AWESOME!.mp3"},
+    {"icon": iconImage, "title": "ROOF THE STREETS - WHEN WE FIND, FIND IN SHINE", "file": "https://github.com/overtunesmusic/overtunesmusic.github.io/raw/main/song/ROOF%20THE%20STREETS%20-%20WHEN%20WE%20FIND,%20FIND%20IN%20SHINE.mp3"},
+    {"icon": iconImage, "title": "Roof The Streets - I Hope, My Way In Time New mx 21", "file": "https://github.com/overtunesmusic/overtunesmusic.github.io/raw/main/song/Roof%20The%20Streets%20-%20I%20Hope%20,%20My%20Way%20In%20Time%20New%20mx%2021.mp3"},
+    {"icon": iconImage, "title": "SELLY OR JELLY - PESTA HARI INI", "file": "https://github.com/overtunesmusic/overtunesmusic.github.io/raw/main/song/SELLY%20OR%20JELLY-%20PESTA%20HARI%20INI.mp3"},
+    {"icon": iconImage, "title": "VISIJISI - ALWAYS BE NIGHTMARE", "file": "https://github.com/overtunesmusic/overtunesmusic.github.io/raw/main/song/VISIJISI%20-%20ALWAYS%20BE%20NIGHTMARE.mp3"},
+    {"icon": iconImage, "title": "Eki Manna & IyoRTML Ft. Mery S - Cintaku Jangan Kau Benci", "file": "https://github.com/overtunesmusic/overtunesmusic.github.io/raw/main/song/Eki%20Manna%20&%20IyoRTML%20Ft.%20Mery%20S%20-%20Cintaku%20Jangan%20Kau%20Benci.mp3"},
+    {"icon": iconImage, "title": "Radio Tip Masa Lampau - CinLok", "file": "https://github.com/overtunesmusic/overtunesmusic.github.io/raw/main/song/Radio%20Tip%20Masa%20Lampau%20-%20CinLok.mp3"}
   ]
 });
